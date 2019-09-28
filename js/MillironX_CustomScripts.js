18a070abee47fd76b5febe1dd5e4a17c7ed88d36
Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com>
2019-09-28 11:44:54 -0700

    Cleanup last of social flair

--- js/MillironX_CustomScripts.js
+++ js/MillironX_CustomScripts.js
@@ -1,5 +1,4 @@
-﻿$(function () { $('#githubFlair').socialFlair('github', 'millironx', {}); });
-$(".img-src").width($("#main-content").width());
+﻿$(".img-src").width($("#main-content").width());
 $(window).resize(function () { $(".img-src").width($("#main-content").width()); });
 $("#nav-link-container").height($("#nav-link-list").height());
 $("#motto").fitText();
