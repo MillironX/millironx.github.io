61dfcafd9ccf13480500b0d80a5c84edf30dcb14
Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com>
2019-09-28 11:00:44 -0700

    Update KeepEPDsReal article

--- js/MillironX_CustomScripts.js
+++ js/MillironX_CustomScripts.js
@@ -3,3 +3,4 @@ $(".img-src").width($("#main-content").width());
 $(window).resize(function () { $(".img-src").width($("#main-content").width()); });
 $("#nav-link-container").height($("#nav-link-list").height());
 $("#motto").fitText();
+$("[rel='tooltip']").tooltip();
