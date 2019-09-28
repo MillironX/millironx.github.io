d419a0c34bd0e386498c754fbb0b651174b06f88
Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com>
2019-09-27 19:56:08 -0700

    Add academia page and fix navigation that it broke

--- js/MillironX_CustomScripts.js
+++ js/MillironX_CustomScripts.js
@@ -1,3 +1,4 @@
-﻿$(function () { $('#githubFlair').socialFlair('github', 'millironx', {}); });
+$(function () { $('#githubFlair').socialFlair('github', 'millironx', {}); });
 $(".img-src").width($("#main-content").width());
-$(window).resize(function () { $(".img-src").width($("#main-content").width()); })
+$(window).resize(function () { $(".img-src").width($("#main-content").width()); });
+$("#nav-link-container").height($("#nav-link-list").height());
