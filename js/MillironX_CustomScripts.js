2f05ea32e00d171de746823691557bf41244ad51
Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com>
2020-09-18 11:45:13 -0600

    Removed unused scripts

--- js/MillironX_CustomScripts.js
+++ js/MillironX_CustomScripts.js
@@ -1,7 +1,14 @@
-﻿$(".img-src").width($("#main-content").width());
+﻿/*!
+ * Custom scripts for millironx.github.io
+ */
+
+// Resize the window image on first page load
+$(".img-src").width($("#main-content").width());
 if ($("#top-container").width() > 768) {
     $("#nav-link-container").height($("#nav-link-list").height());
 }
+
+// Resize the window image if the screen size changes
 $(window).resize(function () {
     $(".img-src").width($("#main-content").width());
     if ($("#top-container").width() > 768) {
@@ -12,5 +19,16 @@ $(window).resize(function () {
     }
 });
 
+// Squeeze the heading text into the proper box
 $("#motto").fitText();
 $("[rel='tooltip']").tooltip();
+
+// Animate the transparency for the glossy scroll effect
+var transparentDemo = true;
+var fixedTop = false;
+
+$(window).scroll(function(e) {
+    oVal = ($(window).scrollTop() / 170);
+    $(".blur").css("opacity", oVal);
+
+});
